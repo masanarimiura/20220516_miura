fd40e9a3ea08fb676479fe81170a259c25c42c24
Masanari Miura <m0s0n0r0h01h01@icloud.com>
2022-05-16 19:54:32 +0900

    提出

---
+++ .history/js/main_20220514034843.js
@@ -0,0 +1,5 @@
+// const target = document.getElementById("menu");
+//target.addEventListener('click', () => {
+//  target.classList.toggle('open');
+//});
+
---
+++ .history/js/main_20220514034911.js
@@ -0,0 +1,11 @@
+// const target = document.getElementById("menu");
+//target.addEventListener('click', () => {
+//  target.classList.toggle('open');
+//});
+
+//const target = document.getElementById("menu");
+//target.addEventListener('click', () => {
+target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
